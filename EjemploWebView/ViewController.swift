9ae589507878205596a61deb2ecb0f24adc1a565
administrador <administrador@maclabserver.local>
2017-02-08 15:11:27 +0100

    Initial Commit

---
+++ EjemploWebView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EjemploWebView
+//
+//  Created by administrador on 8/2/17.
+//  Copyright © 2017 administrador. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
